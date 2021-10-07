7964e68b8530fa003dab6b4d663bf68de6d13c02
Osa <nichita.osadciuc@latoken.com>
2021-10-07 14:15:58 +0300

    update abi encode

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x7ffFc72b22a3A39f471574BEEDC0135767Bf344a";
+export declare const INIT_CODE_HASH = "0xb90691ba2b76d31a708f1044cd9cbfc3201ff667eb93bf0e72e6aa7ea5268f14";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
